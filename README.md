f686308030e3445d6eee35e2c7e7fa6257629b86
Mark Opondo <markopondoi.t@gmail.com>
2026-07-12 13:09:55 +0300

    open food facts api cli interface working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Inventory-management
+The sample barcode used in the cli to search for an item in openfoodfacts is
+
+##  737628064502
--- services/openfoodfact.py
+++ services/openfoodfact.py
@@ -54,4 +54,4 @@ def get_food_facts(barcode):
         }), 502
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
